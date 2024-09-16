6c5ec20b91c8a0bc95f6a0fa89f2f7707a7a1df7
Murilo Dias <mdsouzadias@hotmail.com>
2024-09-16 22:13:28 +0100

    leak fixed in client when the function kill failed, refactoring and documentation

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: mde-souz <mde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 10:05:57 by mde-souz          #+#    #+#             */
-/*   Updated: 2024/09/15 21:30:32 by mde-souz         ###   ########.fr       */
+/*   Updated: 2024/09/16 22:10:14 by mde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,20 @@
 
 int	g_bit_received;
 
-void	send_signal(int sig, pid_t pid)
+static int	send_signal(int sig, pid_t pid)
 {
 	if (kill(pid, sig) == -1)
 	{
-		ft_printf(2, "Kill function failed");
-		exit(EXIT_FAILURE);
+		ft_printf(2, "Kill function failed\n");
+		return (-1);
 	}
+	return (0);
+}
+
+void	free_exit(char *byte)
+{
+	free(byte);
+	exit(EXIT_FAILURE);
 }
 
 void	send_str_with_signal(int pid, char *str)
@@ -28,26 +35,27 @@ void	send_str_with_signal(int pid, char *str)
 	int		j;
 	int		i;
 	char	*byte;
+	int	check_return;
 
 	j = 0;
+	check_return = 0;
 	while (str[j])
 	{
-		byte = ft_char_to_byte(str[j]);
+		byte = ft_char_to_byte(str[j++]);
 		i = 0;
 		while (byte[i])
 		{
 			g_bit_received = 0;
 			if (byte[i++] == '0')
-				send_signal(SIGUSR1, pid);
+				check_return = send_signal(SIGUSR1, pid);
 			else
-				send_signal(SIGUSR2, pid);
+				check_return = send_signal(SIGUSR2, pid);
+			if (check_return == -1)
+				free_exit(byte);
 			while (!g_bit_received)
 				;
-			usleep(50);
 		}
 		free(byte);
-		usleep(50);
-		j++;
 	}
 }
 
@@ -65,9 +73,11 @@ int	main(int argc, char *argv[])
 	if (argc != 3)
 		exit(EXIT_FAILURE);
 	pid_server = ft_atoi(argv[1]);
+	if (pid_server < 0)
+		ft_printf(2, "The process doesn't exist\n");
 	if (signal(SIGUSR1, handler_sigusr) == SIG_ERR)
 		exit(EXIT_FAILURE);
 	send_str_with_signal(pid_server, argv[2]);
-	ft_printf(1, "Mensagem recebida pelo processo pid %d\n", pid_server);
+	ft_printf(1, "The message was received by the process pid %d\n", pid_server);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,13 +6,13 @@
 /*   By: mde-souz <mde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 19:36:21 by mde-souz          #+#    #+#             */
-/*   Updated: 2024/09/15 21:28:46 by mde-souz         ###   ########.fr       */
+/*   Updated: 2024/09/16 22:11:44 by mde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/mini_talk.h"
 
-char	*g_byte;
+static char	*g_byte;
 
 void	print_error_and_exit(char *error)
 {
@@ -31,34 +31,66 @@ void	print_error_and_exit(char *error)
  * So the program can exit in a clean way
  * @param sig The signal number.
  */
-static void handler_sigint(int sig)
+static void	handler_sigint(int sig)
 {
-	free(g_byte);
-	signal(sig, SIG_DFL);
-	kill(SIGINT, getpid());
+	struct sigaction	sa;
+
+	(void)sig;
+	ft_bzero(&sa, sizeof(sa));
+	sa.sa_handler = SIG_DFL;
+	if (sigaction(SIGINT, &sa, NULL) < 0)
+		print_error_and_exit("Failed to set SIGINT to default behavior");
+	if (g_byte)
+	{
+		free(g_byte);
+		g_byte = NULL;
+	}
+	if (kill(getpid(), SIGINT) == -1)
+		print_error_and_exit("kill function failed");
 }
 
+/**
+ * @brief Signal handler function for SIGUSR1 and SIGUSR2 signals,
+ * that receive and print the data that was sent.
+ * 
+ * This function is responsible for handling the SIGUSR1 and SIGUSR2 signals.
+ * It updates the value of the global variable `g_byte` represents
+ * the binary of a number from the ASCII table that is being received by the server.
+ * If the signal is SIGUSR1, the value of `g_byte` is set to '0'.
+ * If the signal is SIGUSR2, the value of `g_byte` is set to '1'.
+ * 
+ * When all the 8 bits are received, the function converts the byte to a character
+ * using the `ft_byte_to_char` function, prints it and sends a SIGUSR1 signal back 
+ * to the process that sent the signal using the `kill` function.
+ * 
+ * @param sig The signal number.
+ * @param si A pointer to a siginfo_t structure containing information about the signal.
+ * @param context A pointer to a ucontext_t structure containing the signal context.
+ */
 static void	handler_sigusr(int sig, siginfo_t *si, void *context)
 {
-	static int	i;
-	
+	static int count;
+
 	(void)context;
 	if (sig == SIGUSR1)
 		*g_byte = '0';
 	else
 		*g_byte = '1';
 	g_byte++;
-	i++;
-	if (i == 8)
+	count++;
+	if (count == 8)
 	{
 		g_byte = g_byte - 8;
 		ft_printf(1, "%c", ft_byte_to_char(g_byte));
-		i = 0;
+		count = 0;
 	}
 	if (kill(si->si_pid, SIGUSR1) == -1)
 		print_error_and_exit("kill function failed");
 }
 
+/**
+ * @brief Keeps the server on, waiting for a signal
+ */
 static void	get_bytes_then_print_string(void)
 {
 	while (1)
@@ -75,6 +107,7 @@ int	main(void)
 	ft_bzero(&sa, sizeof(sa));
 	sa.sa_flags = SA_SIGINFO | SA_RESTART;
 	sa.sa_sigaction = handler_sigusr;
+	sigemptyset(&sa.sa_mask);
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 		print_error_and_exit("Sigaction failed");
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
@@ -82,11 +115,7 @@ int	main(void)
 	if (signal(SIGINT, handler_sigint) == SIG_ERR)
 		print_error_and_exit("Signal failed");
 	ft_printf(1, "SERVER PID: %d\n", getpid());
-	while (1)
-	{
-		get_bytes_then_print_string();
-		ft_printf(1, "\n");
-	}
+	get_bytes_then_print_string();
 	free(g_byte);
 	return (0);
 }
